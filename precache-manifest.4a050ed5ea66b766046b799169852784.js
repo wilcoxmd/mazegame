3e348a7d40257218127a5e5018fefbf0d50a7cc6
Matt Wilcox <wilcox.mattd@gmail.com>
2019-01-21 21:55:58 -0600

    Updates

---
+++ precache-manifest.4a050ed5ea66b766046b799169852784.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = [
+  {
+    "revision": "f5af6c3fb65f7403e9ad000d427a060f",
+    "url": "/mazegame/static/media/goomba.f5af6c3f.png"
+  },
+  {
+    "revision": "c98f0c235d1dd81689c574219f94b6d6",
+    "url": "/mazegame/static/media/Paper-Mario-icon.c98f0c23.png"
+  },
+  {
+    "revision": "8c616ba71cb791a72244",
+    "url": "/mazegame/static/js/runtime~main.8c616ba7.js"
+  },
+  {
+    "revision": "22921c157093340414af",
+    "url": "/mazegame/static/js/main.22921c15.chunk.js"
+  },
+  {
+    "revision": "6c286b01531093c9c127",
+    "url": "/mazegame/static/js/1.6c286b01.chunk.js"
+  },
+  {
+    "revision": "22921c157093340414af",
+    "url": "/mazegame/static/css/main.193198e5.chunk.css"
+  },
+  {
+    "revision": "6843ad22c4a7cc509d017348ef73278d",
+    "url": "/mazegame/index.html"
+  }
+];
